cd8530e5decdb20aab1585807a62284f508ec612
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-27 17:56:00 -0300

    :style: Atualiza o estilo do arquivo rustup.sh

--- rustup.sh
+++ rustup.sh
@@ -1,12 +1,12 @@
 # rust
-export PATH="$PATH:$(brew --prefix)/opt/rustup/bin:$PATH"
-export PATH="${HOME}/.cargo/bin:$PATH"
+export PATH="$PATH:$(brew --prefix)/opt/rustup/bin"
+export PATH="$PATH:${HOME}/.cargo/bin"
 
 eval "$(rustup completions bash)"
 eval "$(rustup completions bash cargo)"
 
 # DEBUG ON
 [ $SCRIPT_DEBUG_ON ] && {
-	echo load file: rustup.sh
-	rustup -V
+    echo load file: rustup.sh
+    rustup -V
 }
