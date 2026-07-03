ddac551b6b2cf85af4110c13e00c07e379bce794
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-07-03 06:17:52 -0300

    :style: Atualiza o estilo do arquivo env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,10 @@
 export PATH=$(brew --prefix)/scripts/bin:$PATH
 export QTFRAMEWORK_BYPASS_LICENSE_CHECK=1
 
+export CARGO_TARGET_DIR=/workspace/.target
+export GRADLE_USER_HOME=/workspace/.gradle
+export CCACHE_DIR=/workspace/.ccache
+export TMPDIR=/workspace/.tmp
+
 # DEBUG ON
 [ $SCRIPT_DEBUG_ON ] && echo load file: env.sh
